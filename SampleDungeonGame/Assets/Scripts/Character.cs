c58e13be943b88f19a8aad40ab8e59d04e909d9f
Kenji Shore <shorekenji@gmail.com>
2018-09-15 11:59:59 -0400

    Changes

---
+++ SampleDungeonGame/Assets/Scripts/Character.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character {
+    GameObject charModel;
+
+    public Character (GameObject model) {
+        charModel = model;
+    }
+
+    void Update (Vector3 direction, Vector3 look) {
+        
+    }
+}
