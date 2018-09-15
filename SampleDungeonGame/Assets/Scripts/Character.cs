2d7438442f6e4d9a542498813d0d0b1adfd6d714
Kenji Shore <shorekenji@gmail.com>
2018-09-15 17:15:36 -0400

    more stuff

--- SampleDungeonGame/Assets/Scripts/Character.cs
+++ SampleDungeonGame/Assets/Scripts/Character.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 
 public class Character {
     GameObject charModel;
+    Rigidbody physics;
 
     public Character (GameObject model) {
         charModel = model;
+        physics = model.GetComponent<Rigidbody>();
+        physics.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
     }
 
-    void Update (Vector3 direction, Vector3 look) {
-        
+    public void Update (Vector3 direction, Vector3 hit) {
+        Vector3 velocity = 10 * direction;
+        physics.velocity = new Vector3(velocity.x, physics.velocity.y, velocity.z);
+
+        Vector3 normal = new Vector3(0, 1, 0);
+        Vector3 look = hit - physics.position;
+        physics.rotation = Quaternion.LookRotation(Vector3.Cross(Vector3.Cross(look, normal), normal), normal);
     }
 }
--- SampleDungeonGame/Assets/temp.cs
+++ SampleDungeonGame/Assets/temp.cs
@@ -4,13 +4,29 @@ using UnityEngine;
 
 public class temp : MonoBehaviour {
 
+    Character controller;
+    Camera cam;
+    Vector3 lastLook;
+
 	// Use this for initialization
 	void Start () {
-		
+        controller = new Character(gameObject);
+        cam = Camera.main;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        
+        Ray mouseRay = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        int layerMask = 1 << 9;
+
+        if (Physics.Raycast(mouseRay, out hit, layerMask)) {
+            lastLook = hit.point;
+        }
+
+        controller.Update(new Vector3(x, 0, y), lastLook);
 	}
 }
