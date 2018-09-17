36652f3ae9813beb57d6552b8829fc37e0b6d793
Kenji Shore <shorekenji@gmail.com>
2018-09-17 16:36:21 -0400

    mats

--- SampleDungeonGame/Assets/Scripts/Character.cs
+++ SampleDungeonGame/Assets/Scripts/Character.cs
@@ -2,22 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Character {
-    public readonly GameObject charModel;
+public class Character
+{
+    public static Transform bullet;
+    public static GameObject charModel;
     readonly Rigidbody physics;
+    readonly float shootTime;
+    readonly float shootDelay;
 
-    public Character (GameObject model) {
+    public Character(GameObject model)
+    {
         charModel = model;
         physics = model.GetComponent<Rigidbody>();
         physics.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+        shootTime = 0;
+        shootDelay = 0.5f;
     }
 
-    public void Update (Vector3 direction, Vector3 hit) {
+    public void Update(Vector3 direction, Vector3 hit)
+    {
         Vector3 velocity = 10 * direction;
         physics.velocity = new Vector3(velocity.x, physics.velocity.y, velocity.z);
 
         Vector3 normal = new Vector3(0, 1, 0);
         Vector3 look = hit - physics.position;
         physics.rotation = Quaternion.LookRotation(Vector3.Cross(Vector3.Cross(look, normal), normal), normal);
+    }
+
+    public void Shoot()
+    {
+        if ((Time.time - shootTime) > shootDelay)
+        {
+
+            //Transform obj = GameObject.Instantiate(bullet, charModel.transform.position, new Quaternion());
+        }
     }
 }
--- SampleDungeonGame/Assets/Scripts/Inputs.cs
+++ SampleDungeonGame/Assets/Scripts/Inputs.cs
@@ -2,34 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Inputs : MonoBehaviour {
+public class Inputs : MonoBehaviour
+{
 
     Character controller;
     CameraClass cam;
     Vector3 lastLook;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         controller = new Character(gameObject);
         cam = new CameraClass(controller, CameraClass.CameraMode.Follow, 10);
         Physics.IgnoreLayerCollision(0, 9);
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
-        
+
         Ray mouseRay = cam.Camera.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         int layerMask = 1 << 9;
 
-        if (Physics.Raycast(mouseRay, out hit, 50, layerMask)) {
-            print(hit.transform.name);
+        if (Physics.Raycast(mouseRay, out hit, 50, layerMask))
+        {
             lastLook = hit.point;
         }
 
         controller.Update(new Vector3(x, 0, y), lastLook);
         cam.Update();
-	}
+
+        if (Input.GetKeyDown("space"))
+        {
+            controller.Shoot();
+        }
+    }
 }
